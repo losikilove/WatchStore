0b0d41ab151330a344f81c3d1f1cdc69722b09bf
losikilove <75561119+losikilove@users.noreply.github.com>
2023-09-23 15:23:46 +0700

    Init server

---
+++ server/src/main/java/dev/phatbeau/watchstore/WatchstoreApplication.java
@@ -0,0 +1,13 @@
+package dev.phatbeau.watchstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchstoreApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/phatbeau/watchstore/WatchstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phatbeau.watchstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
